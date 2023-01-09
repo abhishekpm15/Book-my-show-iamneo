d40466964db13121a8c2ceb22fcaa1ca3b17e552
Abhishek <pabhishekm@gmail.com>
2023-01-09 11:39:40 +0530

    error page check

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -3,7 +3,7 @@ import Error from "../assets/images/404Error.jpg";
 const NotFoundPage = () => {
   return (
     <div className="w-[30%] text-center ml-[32%]">
-      <img src={Error} alt="404-error" />
+      <img src={Error} alt="404-error-page" />
     </div>
   );
 };
