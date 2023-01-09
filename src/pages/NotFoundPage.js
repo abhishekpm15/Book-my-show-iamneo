3094c796d54a0bb157fbd1a26a195e3317666e0f
Abhishek <pabhishekm@gmail.com>
2023-01-09 11:53:35 +0530

    error page check

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -2,8 +2,10 @@ import React from "react";
 import Error from "../assets/images/404Error.jpg";
 const NotFoundPage = () => {
   return (
-    <div className="w-[30%] text-center ml-[32%]">
-      <img src={Error} alt="404-error-page" />
+    <div className="flex justify-content">
+      <div className="w-[30%] text-center ml-[32%]">
+        <img src={Error} alt="404-error-page" />
+      </div>
     </div>
   );
 };
