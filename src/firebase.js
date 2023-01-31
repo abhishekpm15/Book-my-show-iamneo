6de0236af8fad5b810562deec96f63602cd76ec9
Abhishek <pabhishekm@gmail.com>
2023-01-31 12:45:11 +0530

    Errors resolved

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getAuth}from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZs2bfYfW_A9JgKZzpMbeSSpGMh8rDpbg",
-  authDomain: "book-my-show-139be.firebaseapp.com",
-  projectId: "book-my-show-139be",
-  storageBucket: "book-my-show-139be.appspot.com",
-  messagingSenderId: "101557152352",
-  appId: "1:101557152352:web:3267d95533f2c66f497105"
+  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+  authDomain:`${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}` ,
+  projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`
 };
   const app = initializeApp(firebaseConfig);
   export const auth = getAuth(app);
