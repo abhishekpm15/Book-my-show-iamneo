3bf429a90bde4bb1dae43febd1551443a6052e05
Abhishek <pabhishekm@gmail.com>
2023-01-31 12:57:47 +0530

    changes made in firebase file

--- src/firebase.js
+++ src/firebase.js
@@ -2,8 +2,8 @@ import { initializeApp } from "firebase/app";
 import {getAuth}from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZs2bfYfW_A9JgKZzpMbeSSpGMh8rDpbg",
-  authDomain: "book-my-show-139be.firebaseapp.com",
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  authDomain:`${process.env.REACT_APP_AUTH_DOMAIN}` ,
   projectId: "book-my-show-139be",
   storageBucket: "book-my-show-139be.appspot.com",
   messagingSenderId: "101557152352",
